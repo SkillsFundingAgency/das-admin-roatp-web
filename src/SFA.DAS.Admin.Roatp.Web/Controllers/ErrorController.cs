c05e1684b2bf0a0c216ef14e065a74cf51e13a05
Deven Shah <devenshah1974@gmail.com>
2025-09-04 13:58:50 +0100

    CSP-2200 Fixed build warnings

--- src/SFA.DAS.Admin.Roatp.Web/Controllers/ErrorController.cs
+++ src/SFA.DAS.Admin.Roatp.Web/Controllers/ErrorController.cs
@@ -10,26 +10,26 @@ namespace SFA.DAS.Admin.Roatp.Web.Controllers;
 public class ErrorController(IOptions<ApplicationConfiguration> _applicationConfiguration, ILogger<ErrorController> _logger) : Controller
 {
     [Route("{status}")]
+    [HttpGet]
     public IActionResult Index([FromRoute] int status, [FromQuery] string returnUrl)
     {
-        Func<IActionResult> func403 = () =>
-        {
-            if (HttpContext.User != null)
-            {
-                var userName = HttpContext.User.Identity?.Name ?? HttpContext.User.FindFirstValue(ClaimTypes.Upn);
-                var roles = HttpContext.User.Claims.Where(c => c.Type == ClaimTypes.Role || c.Type == Roles.RoleClaimType).Select(c => c.Value);
-
-                _logger.LogError("AccessDenied - User '{UserName}' does not have a valid role. They have the following roles: '{Roles}'", userName, string.Join(",", roles));
-            }
-            var model = new AccessDeniedViewModel(_applicationConfiguration.Value.DfESignInServiceHelpUrl);
-            return View("~/Views/Error/AccessDenied.cshtml", model);
-        };
-
-
         return status switch
         {
-            403 => func403(),
+            403 => Handle403(),
             _ => View("~/Views/Error/ServiceError.cshtml")
         };
+    }
+
+    private IActionResult Handle403()
+    {
+        if (HttpContext.User != null)
+        {
+            var userName = HttpContext.User.Identity?.Name ?? HttpContext.User.FindFirstValue(ClaimTypes.Upn);
+            var roles = HttpContext.User.Claims.Where(c => c.Type == ClaimTypes.Role || c.Type == Roles.RoleClaimType).Select(c => c.Value);
+
+            _logger.LogError("AccessDenied - User '{UserName}' does not have a valid role. They have the following roles: '{Roles}'", userName, string.Join(",", roles));
+        }
+        var model = new AccessDeniedViewModel(_applicationConfiguration.Value.DfESignInServiceHelpUrl);
+        return View("~/Views/Error/AccessDenied.cshtml", model);
     }
 }
--- src/SFA.DAS.Admin.Roatp.Web/Services/ISessionService.cs
+++ src/SFA.DAS.Admin.Roatp.Web/Services/ISessionService.cs
@@ -5,7 +5,7 @@ public interface ISessionService
     void Clear();
     bool Contains(string key);
     void Delete(string key);
-    string Get(string key);
+    string? Get(string key);
     T Get<T>(string key);
     void Set(string key, string value);
     void Set<T>(string key, T model);
