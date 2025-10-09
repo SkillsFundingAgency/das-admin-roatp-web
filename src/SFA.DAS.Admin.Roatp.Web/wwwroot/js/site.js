6eddf875ddb86c6c4a50da7dff5ed3024b84159c
MarkFCain <mark.cain@digital.education.gov.uk>
2025-10-09 16:08:20 +0100

    CSP-2205 implementing back link

--- src/SFA.DAS.Admin.Roatp.Web/wwwroot/js/site.js
+++ src/SFA.DAS.Admin.Roatp.Web/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿
+﻿const $backLinkOrHome = $('.das-js-back-link');
+const backLinkOrHome = function () {
+    const backLink = $('<a>')
+        .attr({ 'href': '#', 'class': 'govuk-back-link' })
+        .text('Back')
+        .on('click', function (e) {
+            window.history.back();
+            e.preventDefault();
+        });
+
+    $backLinkOrHome.replaceWith(backLink);
+
+}
+
+if ($backLinkOrHome) {
+    backLinkOrHome();
+}
