afefe02553aca20ecd942dc4aa1d9afed3811b61
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-07-24 14:01:19 +0100

    CSP-2721 2671 sonar fixes

--- src/SFA.DAS.Admin.Roatp.Web/Extensions/EnumExtensions.cs
+++ src/SFA.DAS.Admin.Roatp.Web/Extensions/EnumExtensions.cs
@@ -1,17 +1,18 @@
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using SFA.DAS.Admin.Roatp.Domain.Models;
 
 namespace SFA.DAS.Admin.Roatp.Web.Extensions;
 
+[ExcludeFromCodeCoverage]
 public static class EnumExtensions
 {
 
     public static string GetDescription(this Enum enumValue)
     {
         return enumValue.GetType()
-                .GetMember(enumValue.ToString())
-                .First()
+                .GetMember(enumValue.ToString())[0]
                 .GetCustomAttribute<DescriptionAttribute>()?
                 .Description ?? string.Empty;
     }
