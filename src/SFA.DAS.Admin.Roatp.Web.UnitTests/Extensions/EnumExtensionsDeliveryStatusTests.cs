5c54880eaf94763b87f49e9273775c5659938b4a
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-07-28 18:10:25 +0100

    CSP-2724 Split EnumExtensionsTests into focused test files

--- src/SFA.DAS.Admin.Roatp.Web.UnitTests/Extensions/EnumExtensionsDeliveryStatusTests.cs
+++ src/SFA.DAS.Admin.Roatp.Web.UnitTests/Extensions/EnumExtensionsDeliveryStatusTests.cs
@@ -1,47 +1,24 @@
 using FluentAssertions;
 using SFA.DAS.Admin.Roatp.Domain.Models;
 using SFA.DAS.Admin.Roatp.Web.Extensions;
-using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
 
 namespace SFA.DAS.Admin.Roatp.Web.UnitTests.Extensions;
 
 [TestFixture]
-public class EnumExtensionsTests
+public class EnumExtensionsDeliveryStatusTests
 {
-    [TestCase(LearningType.Apprenticeship, "Apprenticeship")]
-    [TestCase(LearningType.ApprenticeshipUnit, "Apprenticeship unit")]
-    [TestCase(LearningType.FoundationApprenticeship, "Foundation apprenticeship")]
-    public void GetDescription_ReturnsExpectedDescription(LearningType learningType, string expected)
-    {
-        learningType.GetDescription().Should().Be(expected);
-    }
-
-    [Test]
-    public void GetDescription_ReturnsEmptyString_WhenNoDescriptionAttribute()
-    {
-        TestEnum.ItemWithoutDescription.GetDescription().Should().BeEmpty();
-    }
-
-    [TestCase(LearningType.Apprenticeship, "govuk-tag--blue")]
-    [TestCase(LearningType.FoundationApprenticeship, "govuk-tag--pink")]
-    [TestCase(LearningType.ApprenticeshipUnit, "govuk-tag--purple")]
-    public void GetTagClass_ReturnsExpectedClass(LearningType learningType, string expected)
-    {
-        learningType.GetTagClass().Should().Be(expected);
-    }
-
-    [Test]
-    public void GetTagClass_ReturnsEmptyString_ForUnknownValue()
-    {
-        ((LearningType)999).GetTagClass().Should().BeEmpty();
-    }
-
     [TestCase(DeliveryStatus.OpenToNewStarts, "govuk-tag--green")]
     [TestCase(DeliveryStatus.LastStartDateAdded, "govuk-tag--orange")]
     [TestCase(DeliveryStatus.ClosedToNewStarts, "govuk-tag--grey")]
     public void WhenGettingTagClass_AndDeliveryStatusProvided_ThenReturnsExpectedClass(DeliveryStatus deliveryStatus, string expected)
     {
         deliveryStatus.GetTagClass().Should().Be(expected);
+    }
+
+    [Test]
+    public void WhenGettingTagClass_AndUnknownValue_ThenReturnsEmptyString()
+    {
+        ((DeliveryStatus)999).GetTagClass().Should().BeEmpty();
     }
 
     [Test]
@@ -64,12 +41,5 @@ public class EnumExtensionsTests
         var today = new DateTime(2026, 7, 27);
         DateTime? pastDate = today.AddDays(-1);
         pastDate.ToDeliveryStatus(today).Should().Be(DeliveryStatus.ClosedToNewStarts);
-    }
-
-    private enum TestEnum
-    {
-        [Description("Described")]
-        ItemWithDescription,
-        ItemWithoutDescription
     }
 }
