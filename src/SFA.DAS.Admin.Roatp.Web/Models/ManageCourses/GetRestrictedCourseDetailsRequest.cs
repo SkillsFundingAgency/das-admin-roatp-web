382715754fefd4c6900cb8594708d2eddc829c85
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-07-30 16:27:45 +0100

    CSP-2726 Removed [FromQuery] from SearchTerm, DeliveryStatus props, these properties are no longer bound from query string parameters in HTTP requests.

--- src/SFA.DAS.Admin.Roatp.Web/Models/ManageCourses/GetRestrictedCourseDetailsRequest.cs
+++ src/SFA.DAS.Admin.Roatp.Web/Models/ManageCourses/GetRestrictedCourseDetailsRequest.cs
@@ -1,14 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Admin.Roatp.Domain.Models;
 
 namespace SFA.DAS.Admin.Roatp.Web.Models.ManageCourses;
 
 public class GetRestrictedCourseDetailsRequest
 {
-    [FromQuery]
     public string SearchTerm { get; set; } = string.Empty;
 
-    [FromQuery]
     public List<DeliveryStatus> DeliveryStatus { get; set; } = [];
 
     public bool HasSearchTermFilter => !string.IsNullOrWhiteSpace(SearchTerm);
